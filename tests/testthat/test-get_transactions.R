31ac76d01c744104ca29682924deb8f3bbd06bd0
lorenzbr <lorenz.brachtendorf@gmx.de>
2022-03-29 14:05:30 +0200

    rename test file

--- tests/testthat/test-get_transactions.R
+++ tests/testthat/test-get_transactions.R
@@ -2,8 +2,7 @@ files <- list.files(system.file("extdata", package = "BankStatementParser"),
                     pattern = "pdf", full.names = TRUE)
 document_names <- basename(files)
 
-list_dfs <- mapply(BankStatementParser::get_transactions, files, 
-                   document_names, SIMPLIFY = FALSE)
+list_dfs <- mapply(get_transactions, files, document_names, SIMPLIFY = FALSE)
 df_transactions <- do.call(rbind, unname(list_dfs))
 
 test_that("columns in data frame are not NA", {
